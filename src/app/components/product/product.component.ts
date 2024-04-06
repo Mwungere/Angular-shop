189903de601aa468cd0739a5085d99206b27fa23
Mwungere <mwungereelite56@gmail.com>
2024-04-06 19:42:51 +0200

    delete

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,46 @@
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Product } from '../../../types';
+import { RatingModule } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { ConfirmationService } from 'primeng/api';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { ToastModule } from 'primeng/toast';
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [RatingModule, FormsModule, ButtonModule, ConfirmPopupModule, ToastModule],
+  providers: [ConfirmationService],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.scss'
+})
+export class ProductComponent {
+  constructor(private confirmService: ConfirmationService){}
+  @ViewChild('deleteButton') deleteButton:any
+  @Input() product!: Product;
+  @Output() edit: EventEmitter<Product> = new EventEmitter<Product>();
+  @Output() delete: EventEmitter<Product> = new EventEmitter<Product>();
+
+  editProduct(){
+    this.edit.emit(this.product);
+
+  }
+
+  confirmDelete(){
+    this.confirmService.confirm({
+      target: this.deleteButton.nativeElement,
+      message: 'Are you sure you want to delete this product?',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.deleteProduct()
+      }
+    });
+  }
+
+  deleteProduct(){
+    this.delete.emit(this.product);
+  }  
+  ngOnInit() {
+  }
+  
+}
