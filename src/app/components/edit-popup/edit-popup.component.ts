30886394cd7d5d61874166ef1fed822d01ba409d
Mwungere <mwungereelite56@gmail.com>
2024-04-07 00:19:00 +0200

    validator

--- src/app/components/edit-popup/edit-popup.component.ts
+++ src/app/components/edit-popup/edit-popup.component.ts
@@ -2,18 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { Product } from '../../../types';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { RatingModule } from 'primeng/rating';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-edit-popup',
   standalone: true,
-  imports: [DialogModule, CommonModule, FormsModule, RatingModule, ButtonModule],
+  imports: [DialogModule, CommonModule, FormsModule, RatingModule, ButtonModule, ReactiveFormsModule],
   templateUrl: './edit-popup.component.html',
   styleUrl: './edit-popup.component.scss'
 })
 export class EditPopupComponent {
+  constructor(private formBuilder: FormBuilder){}
   @Input() display: boolean = false;
   @Output() displayChange = new EventEmitter<boolean>();
   @Input() header!: string;
@@ -24,8 +25,32 @@ export class EditPopupComponent {
     rating: 0
   }
   @Output() confirm= new EventEmitter<Product>();
+
+  specialCharacterValidator(): ValidatorFn {
+    return (control) => {
+      const hasSpecialCharacter = /[!@#$%^&*()_\-=\[\]{};':"\\|,.<>\/?]+/.test(
+        control.value
+      );
+      return hasSpecialCharacter ? { hasSpecialCharacter: true } : null;
+    }
+  }
+
+  productForm = this.formBuilder.group({
+    price: ['', [Validators.required]],
+    name: ['', [Validators.required, this.specialCharacterValidator()]],
+    image: [''],
+    rating: [0]
+  });
+  ngOnChanges(){
+    this.productForm.patchValue(this.product)
+  }
   onConfirm(){
-    this.confirm.emit(this.product);
+    this.confirm.emit({
+      price: this.productForm.value.price || '',
+      name: this.productForm.value.name || '',
+      image: this.productForm.value.image || '',
+      rating: this.productForm.value.rating || 0
+    });
     this.display = false;
     this.displayChange.emit(this.display);
   }
---
+++ src/app/modules/about-us/about-us-2/about-us-2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-us-2',
+  standalone: true,
+  imports: [],
+  templateUrl: './about-us-2.component.html',
+  styleUrl: './about-us-2.component.scss'
+})
+export class AboutUs2Component {
+
+}
